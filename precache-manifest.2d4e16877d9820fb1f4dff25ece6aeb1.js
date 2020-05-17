ecfe11a5320d42bc89e5eab6641dd44a1b44d16e
Aj <jayanwana@gmail.com>
2020-05-17 21:23:41 +0100

    initial commit

---
+++ precache-manifest.2d4e16877d9820fb1f4dff25ece6aeb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949a0d8498aede1ab8129b73305b4b23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b786ed7a377e0a57df6c",
+    "url": "/static/css/main.c0c756c0.chunk.css"
+  },
+  {
+    "revision": "db284d1dafce010da501",
+    "url": "/static/js/2.47da3053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47da3053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b786ed7a377e0a57df6c",
+    "url": "/static/js/main.b5d57aba.chunk.js"
+  },
+  {
+    "revision": "39e1f84063178bdbceba",
+    "url": "/static/js/runtime-main.faac8865.js"
+  }
+]);
